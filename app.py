536b6cc3a2ba6599e27be420c6f52feeb5d94b52
Caio Moreira <caiomoreirafreitas@gmail.com>
2026-02-04 16:38:55 -0300

    criação do arquivo app.py com leitura de dados em dataframe

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# abrir arquivo de dados em um DataFrame
+df_vehicles = pd.read_csv(r"C:\Users\ADM\TripleTen\TT_Sprint5\vehicles.csv")
